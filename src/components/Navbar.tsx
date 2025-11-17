6d4dee289432bed09b6c33bbd291f1afa5410213
Jannatul <limajannat1675@gmail.com>
2025-11-18 01:25:53 +0600

    update error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -80,11 +80,11 @@ export default function Navbar() {
 
       {/* Main Navigation */}
       <header className="bg-white shadow-lg sticky top-0 z-50">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             {/* Logo */}
             <Link href="/" className="flex items-center">
-              <div className="text-2xl font-bold text-green-600">grocery</div>
+              <div className="text-2xl font-bold text-green-600">KachaBazer</div>
             </Link>
 
             {/* Desktop Navigation */}
